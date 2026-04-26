c173a56663c92cf828805e8718844cdf194170d0
Evandro Francisco <evandr283@gmail.com>
2026-04-26 20:31:53 +0100

    Correção de estrutura e configuração do banco PostgreSQL

--- front-end/static/js/stock.js
+++
@@ -1,7 +0,0 @@
-function mostrar_div(){
-    const div = document.getElementById("modalVenda");
-    div.style.display = 'block';
-}
-function fecharModal() {
-    document.getElementById('modalVenda').style.display = 'none';
-}
